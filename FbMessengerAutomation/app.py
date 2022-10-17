10cf84abd652921d152ebf6bdaa11a6297e98ecd
MamunCUETCSE <112986954+MamunCUETCSE@users.noreply.github.com>
2022-10-17 16:01:32 +0600

    Add files via upload

---
+++ FbMessengerAutomation/app.py
@@ -0,0 +1,10 @@
+import time
+
+import pyautogui as pg
+while True:
+    time.sleep(10)
+    pg.typewrite('Hi am working with fb automation')
+    pg.press('enter')
+    time.sleep(10)
+    pg.typewrite('please donot mind having this message')
+    pg.press('enter')
